6b105146038e16a4840293928d0b223f9f9d3a2f
kielsonzinn <kielsonzinn@gmail.com>
2023-07-06 14:22:57 -0300

    add port param

--- database/database.py
+++ database/database.py
@@ -6,9 +6,10 @@ def connection():
     db_host = os.environ.get('DB_HOST')
     db_name = os.environ.get('DB_NAME')
     db_user = os.environ.get('DB_USER')
+    db_port = os.environ.get('DB_PORT')
     db_password = os.environ.get('DB_PASSWORD')
 
-    return psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=db_password)
+    return psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=db_password, port=db_port)
 
 
 def migration():
